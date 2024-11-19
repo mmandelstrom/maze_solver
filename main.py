1c8e2b28ec76680ee8996e408626a4126d0ff4e0
mmandelstrom <mmandelstrom@gmail.com>
2024-11-19 11:47:00 +0100

    Renamed window > graphics, added Point and Line classes with draw method for Line

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from window import Window
+from graphics import Window, Point, Line
 
 win = Window(800, 600)
+
+l = Line(Point(50, 50), Point(0, 800))
+win.draw_line(l, "red")
+
+
+
 win.wait_for_close()
