0a0dcc4b0a89c5c3d18973d7707d77b5f1f3cde5
mmandelstrom <mmandelstrom@gmail.com>
2024-11-26 10:16:16 +0100

    Added method to break entrance and exit and test for it

--- cell.py
+++ cell.py
@@ -20,18 +20,39 @@ class Cell:
         self._x2 = x2
         self._y1 = y1
         self._y2 = y2
+        if self._win is None:
+            return
+        
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
+
         if self.has_top_wall:
             line = Line(Point(x1, y1), Point(x2, y1))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
+
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
+
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
+
+        
+        
 
 #Gets center x / y from x1,x2,y1,y2 of both cells then calls draw_line between them
     def draw_move(self, to_cell, undo=False):
